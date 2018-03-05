3eeca2a92c2c0aaaf76502b42404d4ef33b6c10d
Wilson, Andrew <36511382+luxarcanum@users.noreply.github.com>
2018-03-05 22:39:59 +0000

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBooks.Web.Models
+{
+    public class Book
+    {
+        public virtual long Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Author { get; set; }
+        public virtual string Genre { get; set; }
+        public virtual string Review { get; set; }
+    }
+}
---
+++ NhibernateSession.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBooks.Web
+{
+    public class NHibernateSession
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var bookConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Mappings\Book.hbm.xml");
+            configuration.AddFile(bookConfigurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
